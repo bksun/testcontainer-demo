b4023986062765d9d6ae476060d1f5d9881a53b5
Sunil Singh <sunil.singh1@walmart.com>
2021-12-21 20:57:33 +0530

    testcontainer with mysql

--- src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
+++ src/test/java/com/jetbrains/testcontainersdemo/CustomerIntegrationTests.java
@@ -3,16 +3,43 @@ package com.jetbrains.testcontainersdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
+
+
+@SpringBootTest(classes = TestcontainersDemoApplication.class, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Testcontainers
 public class CustomerIntegrationTests {
 
     @Autowired
     private CustomerDao customerDao;
+
+//    @Autowired
+//    public CustomerIntegrationTests(final CustomerDao customerDao) {
+//        this.customerDao = customerDao;
+//    }
+
+    @Container
+    private static MySQLContainer container = new MySQLContainer("mysql:8.0.26");
+//            .withDatabaseName("somedatabase")
+//            .withUsername("root")
+//            .withPassword("letsgomarco");
+
+    @DynamicPropertySource
+    public static void overrideProps(final DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.username", container::getUsername);
+        registry.add("spring.datasource.password", container::getPassword);
+
+    }
 
     @Test
     void when_using_a_clean_db_this_should_be_empty() {
